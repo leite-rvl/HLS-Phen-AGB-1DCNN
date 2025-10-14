e222ef6b6229c8e722ece83dcdd741ae0d8c767b
rodrigo.leite <rodrigovl.eng.florestal@gmail.com>
2025-10-13 18:10:08 -0700

    zonal stats change

--- timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
+++ timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
+# Get current location of build script
+basedir=$(dirname "$(readlink -f "$0")")
 
 # Make default dir the .sh command dir
 # SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
