96d5e0625283fc7f12377bd20c3acdce8417ab50
rodrigo.leite <rodrigovl.eng.florestal@gmail.com>
2025-10-13 19:20:40 -0700

    zonal stats change

--- timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
+++ timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
@@ -31,19 +31,19 @@ DIR_IMG=$2
 OUTPUT=$3
 
 
-python /timeseries_modeling/zonal_stats_vrt_list/zonal_stats_vrt.py \
-  --gpkg       "${GPKG}" \
-  --dir-img    "${DIR_IMG}" \
-  --output     "${OUTPUT}" \
-  --zone-id-col "${ZONE_ID_COL:-zone}" \
-  --buffer-m    "${BUFFER_M:-0}" \
-  --exclude-bands ${EXCLUDE_BANDS:-ValidMask.vrt count.vrt}
-
-
-# python zonal_stats_vrt.py \
+# python /timeseries_modeling/zonal_stats_vrt_list/zonal_stats_vrt.py \
 #   --gpkg       "${GPKG}" \
 #   --dir-img    "${DIR_IMG}" \
 #   --output     "${OUTPUT}" \
 #   --zone-id-col "${ZONE_ID_COL:-zone}" \
 #   --buffer-m    "${BUFFER_M:-0}" \
 #   --exclude-bands ${EXCLUDE_BANDS:-ValidMask.vrt count.vrt}
+
+
+python zonal_stats_vrt.py \
+  --gpkg       "${GPKG}" \
+  --dir-img    "${DIR_IMG}" \
+  --output     "${OUTPUT}" \
+  --zone-id-col "${ZONE_ID_COL:-zone}" \
+  --buffer-m    "${BUFFER_M:-0}" \
+  --exclude-bands ${EXCLUDE_BANDS:-ValidMask.vrt count.vrt}
