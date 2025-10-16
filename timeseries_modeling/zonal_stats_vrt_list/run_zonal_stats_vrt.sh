0bf938c08bd96ce8d3dcd4372ebcec2dadb11429
rodrigo.leite <rodrigovl.eng.florestal@gmail.com>
2025-10-16 12:09:15 -0700

    update zonal stats

--- timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
+++ timeseries_modeling/zonal_stats_vrt_list/run_zonal_stats_vrt.sh
@@ -18,7 +18,7 @@ cd "$SCRIPT_DIR"
 
 
 # TESTING
-GPKG="/projects/my-private-bucket/HLS-1DCNN-AGB/data/shp/gedi/test/l4a_t90km_t89_veg2022_outrm.gpkg" 
+GPKG="https://maap-ops-workspace.s3.amazonaws.com/rodrigo.leite/HLS-1DCNN-AGB/data/shp/gedi/test/l4a_t90km_t89_veg2022_outrm.gpkg" 
 DIR_IMG="/projects/my-private-bucket/HLS-1DCNN-AGB/data/tif/HLS_composites/monthly/br_af_grid60km_prj_evi2_max/vrt_test/" 
 OUTPUT="/projects/my-private-bucket/HLS-1DCNN-AGB/data/shp/gedi/l4a_t90km_t89_veg2022_outrm_zonal_HLS.gpkg" 
 ZONE_ID_COL="zone" 
